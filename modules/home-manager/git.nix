2715e9f3979fa7b462f156aaf8917fe3895eb94c
recluse <recluse0code@gmail.com>
2025-09-12 18:16:15 +0530

    removed the redundant importDir function

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -2,7 +2,7 @@
   programs.git = {
     enable = true;
     userEmail = "recluse0code@gmail.com";
-    userName = "recluse";
+    userName = "recluse0";
     extraConfig = {
       credential.helper = "store"; 
       core.askpass = ""; # Any other git config settings
--- modules/importDir.nix
+++
@@ -1,2 +0,0 @@
-dir: map (x: dir + "/${x}") (builtins.filter (name: name != "default.nix") (builtins.attrNames (builtins.readDir dir)))
-
